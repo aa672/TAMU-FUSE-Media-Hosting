9da98fcb4a78f26b17b421c19205241c8c5d4297
adrianaguirre <adrianaguirre@tamu.edu>
2022-09-18 16:39:40 -0500

    changed root to pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :pages
   resources :contents
   resources :modules
-  root 'modules#index'
+  root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
