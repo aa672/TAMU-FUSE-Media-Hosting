5323779d7c98e444a72480b5c6bfcbf75a626e7c
rgovat <rkg0094@tamu.edu>
2022-10-17 17:40:38 -0500

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ ModuleSection.create!([
   {module_name: "Mod 2"}
 ])
 Page.create!([
-  {content_id: 1, page_name: "Page 1", page_description: "Desc 1"},
-  {content_id: 2, page_name: "Page 2", page_description: "Desc 2"}
+  {module_id: 1, content_id: 1, page_name: "Page 1", page_description: "Desc 1"},
+  {module_id: 2, content_id: 2, page_name: "Page 2", page_description: "Desc 2"}
 ])
