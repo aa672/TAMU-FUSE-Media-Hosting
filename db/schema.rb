d9008a573787e084444b3ff8e99c2161ad5c9f54
Adrian Aguirre <67935654+aa672@users.noreply.github.com>
2022-11-09 17:41:55 -0600

    update gemlock

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_01_001953) do
+ActiveRecord::Schema.define(version: 2022_11_06_221106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2022_11_01_001953) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "content_ids", default: [], array: true
+    t.string "module_name"
   end
 
   create_table "passwords", force: :cascade do |t|
