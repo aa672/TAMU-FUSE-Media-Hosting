51d2b92930db0460ee21717fe5df818604fce292
Adrian Aguirre <67935654+aa672@users.noreply.github.com>
2022-11-09 18:28:56 -0600

    change to seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,3 @@ ModuleSection.create!([
   {module_name: "Mod name"},
   {module_name: "Mod 2"}
 ])
-Page.create!([
-  {module_name: "Mod name", content_id: 1, page_name: "Page 1", page_description: "Desc 1"},
-  {module_name: "Mod 2", content_id: 2, page_name: "Page 2", page_description: "Desc 2"}
-])
