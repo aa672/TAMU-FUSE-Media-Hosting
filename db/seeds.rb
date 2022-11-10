ea5c3c0b67f9a741a4b4c0527e21ee791e389dce
root <carterho1103@tamu.edu>
2022-11-10 07:24:57 +0000

    basic user system created

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,3 @@ ModuleSection.create!([
   {module_name: "Mod name"},
   {module_name: "Mod 2"}
 ])
-Page.create!([
-  {module_id: 1, content_id: 1, page_name: "Page 1", page_description: "Desc 1"},
-  {module_id: 2, content_id: 2, page_name: "Page 2", page_description: "Desc 2"}
-])
