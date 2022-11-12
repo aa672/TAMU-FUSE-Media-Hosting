dd04694304d508bfc214cbaede982fb6a999906c
Adrian Aguirre <67935654+aa672@users.noreply.github.com>
2022-11-12 16:27:27 -0600

    Added more bootstrap to most pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,3 @@
 class Page < ApplicationRecord
-    validates :module_id, :content_ids, :page_name, :page_description, presence: true
+    validates :module_name, :content_ids, :page_name, :page_description, presence: true
 end
