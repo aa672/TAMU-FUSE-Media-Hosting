c3bb00c9ad0b647dad2391997da0148dda4136d0
Adrian Aguirre <67935654+aa672@users.noreply.github.com>
2022-11-14 21:42:33 -0600

    small update

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,5 +2,5 @@ class Page < ApplicationRecord
     has_many :content_tags, dependent: :destroy
     has_many :tags, through: :content_tags
 
-    validates :module_name, :page_name, presence: true
+    validates :page_name, presence: true
 end
